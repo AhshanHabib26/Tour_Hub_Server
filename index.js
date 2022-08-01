b0f1dc5df2d7975fcf547f05e9b49bfc652a9f41
AhshanHabib26 <habib.net27@gmail.com>
2022-08-01 13:05:03 +0600

    Tour Hub Server

---
+++ index.js
@@ -0,0 +1,75 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+const User = require("./user/user");
+const { hashPassword, comparePassword } = require("./helpers/helpers");
+const jwt = require("jsonwebtoken");
+
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.nrtffnx.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB CONNECTION ERROR: ", err));
+
+app.use(express.json({ limit: "4mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World! I am Coming From TourHub Server");
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name) {
+      return res.json({
+        error: "Name is required",
+      });
+    }
+    if (!email) {
+      return res.json({
+        error: "Email is required",
+      });
+    }
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "Password is required and should be 6 characters long",
+      });
+    }
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "Email is taken",
+      });
+    }
+    const hashedPassword = await hashPassword(password);
+
+    try {
+      const user = await new User({
+        name,
+        email,
+        password: hashedPassword,
+      }).save();
+
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      const { password, ...rest } = user._doc;
+      return res.json({
+        token,
+        user: rest,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(4000, () => console.log("Server Running On Port 4000"));
